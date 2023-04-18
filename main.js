fd1702e8a3f061c87f77f3e3b6c561ce79bba70a
Alec Livaditis <aleclivaditis@gmail.com>
2023-04-18 15:43:48 -0400

    add createGame function

--- main.js
+++ main.js
@@ -11,7 +11,14 @@ function takeTurn() {
 
 }
 
-function createGame() {
+function createGame(player1, player2) {
+  var game = {
+    player1,
+    player2,
+    player1Choice: null,
+    player2Choice: null,
+    gameType: null
+  }
 }
 
 function checkWinCondition() {
