30a7f969e2d5de57414913c3e41a7dfca13601a8
Alec Livaditis <aleclivaditis@gmail.com>
2023-04-18 15:07:08 -0400

    add createPlayer function, add other functions that need to be programmed

---
+++ main.js
@@ -0,0 +1,27 @@
+function createPlayer(playerName) {
+  var player = {
+    playerName,
+    token: '👴🏻',
+    wins: 0
+  }
+  return player
+}
+
+function takeTurn() {
+
+}
+
+function createGame() {
+}
+
+function checkWinCondition() {
+
+}
+
+function detectDraw() {
+
+}
+
+function resetGame() {
+  
+}
